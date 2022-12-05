d2e5576424dd45300d76c6f696931f27df2df2a8
Yanggz <350926623@qq.com>
2022-12-05 11:05:37 +0800

    UI样式

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import copyTxt from "./util/clipborad";
 import './index.scss';
 
 netIP();
-
+// 下载按钮
 $(".downloadBtn").on('click', () => {
   copyTxt('.downloadBtn')
 })
+
+// 关闭推荐弹框
+$(".mask, .popupClose").on('click', (e) => {
+  $("#popup").attr("style", "display: none");
+  e.preventDefault();
+})
+
 
 window.onload = function () {
   netHiveLog({ action: 1 }, 'eventType_pv')
@@ -14,6 +21,7 @@ window.onload = function () {
   // $(".downloadBtn").on('click', () => {
   //   copyTxt('.downloadBtn')
   // })
+
 }
 
 
