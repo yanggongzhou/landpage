c5e4a7d677b078ded05a7a8909dd97a22abcfdb5
Yanggz <350926623@qq.com>
2022-12-07 12:30:30 +0800

    环境变量

--- config/outFileMerge.js
+++ config/outFileMerge.js
@@ -1,5 +1,17 @@
 const fs = require('fs');
 const path = require("path")
+const npmLifecycleEvent = process.env.npm_lifecycle_event
+console.log('npm_lifecycle_event------>', npmLifecycleEvent)
+
+//'https://hwyc-res-qat.s3.ap-southeast-1.amazonaws.com';
+// 'https://landpage.hw.dzods.cn'
+let BASE_URL = 'https://landpage.hw.dzods.cn'
+// let BASE_URL = 'https://hwyc-res-qat.s3.ap-southeast-1.amazonaws.com';
+// if (npmLifecycleEvent === 'build-test') {
+//   BASE_URL = 'https://landpage.hw.dzods.cn';
+// } else if (npmLifecycleEvent === 'build-stag') {
+//  
+// }
 
 let platforms = []
 const files = fs.readdirSync('./platform')
@@ -10,29 +22,27 @@ files.forEach(function (item, index) {
   }
 })
 
-const jsFile = fs.readFileSync('./dist/index.js', 'utf8').replace('//# sourceMappingURL=index.js.map', '//# sourceMappingURL=https://landpage.hw.dzods.cn/landpage_model/v3/index.js.map')
-console.log('读取jsFile-------------->')
+const jsFile = fs.readFileSync('./dist/index.js', 'utf8').replace('//# sourceMappingURL=index.js.map', `//# sourceMappingURL=${BASE_URL}/landpage_model/v3/index.js.map`)
+console.log('--------读取jsFile------')
 const cssFile = fs.readFileSync('./dist/main.css', 'utf8')
-console.log('读取cssFile-------------->')
+console.log('-------读取cssFile-—-----')
 for (let i = 0; i < platforms.length; i++) {
   const plat = platforms[i]
   const filepath = path.join(__dirname, `../dist/${plat}.html`)
   const htmlFile = fs.readFileSync(filepath, 'utf8')
-  console.log('读取htmlFile-------------->')
-
+  console.log('-------读取htmlFile-—-----')
   const replaceFile = htmlFile.replace(/<script defer src="index.js?[^*>]*><\/script[^>]*>/, 'replace_bandle_script')
     .replace('replace_bandle_script', `<script id="bandle" defer type="text/javascript">${jsFile}</script>`)
     .replace(/<link href="main.css?[^*>]* rel="stylesheet">/, `<style type="text/css">${cssFile}</style>`)
-  console.log('替换js css map-------------->')
+  console.log('------替换js css map-—----')
   // fs.writeFile  写入文件（会覆盖之前的内容）（文件不存在就创建）  utf8参数可以省略
   fs.writeFile(filepath, replaceFile, 'utf8', function (error) {
     if (error) {
       console.log(error);
       return false;
     }
-    console.log('写入成功', plat);
+    console.log('------写入成功-—----', plat)
   })
-
 }
 
 
